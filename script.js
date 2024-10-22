b2e65187514049319dc31c0017ba3562927baa95
Iunes Al Akhras <alakhrasyunespr1@gmail.com>
2024-10-22 16:26:03 +0300

    Final Version (I hope)

--- script.js
+++ script.js
@@ -2,35 +2,61 @@
 var language = localStorage.getItem("language");
 
 var enJson = {
-  "nav": {
-    "about" : "About Me", 
-    "education" : "Education",
-    "skills" : "Skills",
-    "experience" : "Work Experience",
+  nav: {
+    about: "About Me",
+    education: "Education",
+    skills: "Skills",
+    experience: "Work Experience",
   },
-  "section": {
-    "aboutSection": "<h2>About me</h2><p> I am currently in the second year of my BSc in informatics at the University of Strasbourg. My interest is focused on the fields of bioinformatics, <strong>data science and operating system development</strong>. I already have a little experience as a bioinformatician, which convinced me that I am moving in the right direction and made me more adaptable to different environments </p>",
-    "educationSection": "<h2>Education</h2><li><dt>School Number 27</dt><dd>Petrozavodsk / 2008-2019<br>Vocational in chemistry and biology<br>Competences aqcuired: basic informatics, biology</dd><dt>University of Strasbourg</dt><dd>Strasbourg / 2019-2022<br>BSc in Molecular and Cellular Biology<br>Competences acquired: Python, MS Office, Linux, centrifugation, chromotography, electrophoresis</dd><br><dd>Strasbourg / 2023-now (winter semester)<br>BSc in Informatics<br>Competences acquired: Python, C, Algorithmics, Javascript, HTML, CSS, linear algebra, Linux</dd></li>",
-    "skillsSection": "<h2>Skills</h2><li>  <dt>Informatics:</dt>  <dd>Python, Javascript, HTML, CSS, Haskell, C</dd>  <dt>Bioinformatics:</dt>  <dd>Biopython, EBI and NCBI services, PDB</dd>  <dt>Data Science:</dt>  <dd>Linear Algebra, Statistics, R, Pandas</dd>  <dt>Languages:</dt>  <dd>English (C1), French(B2+), German(B2), Russian(Native)</dd>  <dt>Others:</dt>  <dd>OnlyOffice, Google Office</dd></li>",
-    "experienceSection": "<h2>My work experience</h2><li>  <dt>Internship in Bioinformatics and Molecular Biology</dt>  <dd>CNRS / September-December 2021<br> Mostly oriented towards bioinformatics, this internship permitted me to improve my knowledge of Python and different services used in bioinformatics</dd>  <dt>Internship in Bioinformatics</dt>  <dd>CNRS / September-December 2022<br> I worked in the same group as the previous time, using and improving the same competences and additionally gaining structural biology knowledge</dd>  <dt>Django Developer</dt>  <dd>Consilio / June-July 2022<br>My first experience in web. We developed a site for a small startup using Django as back-end</dd></li>",
-  }
-  
-}
+  section: {
+    aboutSection:
+      "<h2>About Me</h2> <p>Currently, I am a first-year master's student in the field of applied informatics at the Moscow International Academy. I am interested in areas such as bioinformatics, data science, and operating system development. I already have some experience working as a bioinformatician/analyst, which convinced me that I am moving in the right direction and allowed me to better adapt to various conditions.</p> ",
+    educationSection:
+      '<h2>Education</h2><li><dt>School No. 27</dt><dd>Petrozavodsk / 2008-2019<br>Chemical-Biological Profile<br>Acquired Competencies: basic informatics, biology</dd><dt>University of Strasbourg</dt><dd>Strasbourg (France) / 2019-2022<br>Bachelor\'s in "Molecular and Cellular Biology"<br>Acquired Competencies: Python, MS Office, Linux, basic statistics, centrifugation, chromatography, electrophoresis</dd><br><dd>Strasbourg (France) / 2023-2024<br>Additional education in informatics<br>Acquired Competencies: Python, C, algorithms, Javascript, HTML, CSS, linear algebra, Linux</dd></li>',
+    skillsSection:
+      "<h2>Skills</h2><li><dt>Informatics:</dt><dd>Python, HTML, CSS, Haskell, C/C++</dd><dt>Bioinformatics:</dt><dd>biopython, EBI and NCBI services, PDB</dd><dt>Data Science:</dt><dd>linear algebra, statistics, R, pandas</dd><dt>Languages:</dt><dd>English (C1), French (B2+), German (B2), Russian (native)</dd><dt>Others:</dt><dd>OnlyOffice, Google Office</dd></li>",
+    experienceSection:
+      "<h2>Work Experience</h2><li><dt>Bioinformatics Internship</dt><dd>CNRS / September-December 2021<br>Mainly focused in the field of bioinformatics, this internship helped me improve my understanding of Python and bioinformatics</dd><dt>Bioinformatics Internship</dt><dd>CNRS / September-December 2022<br>Essentially a continuation of the previous internship, it allowed me to further deepen my skills and knowledge of protein biology</dd><dt>Django Developer</dt><dd>Consilio / June-July 2022<br>My first experience in web development. We developed the backend for a small startup</dd></li>",
+  },
+};
 
 var frJson = {
-  "nav" : {
-    "about" : "À propos de moi",
-    "education" : "Formation",
-    "skills" : "Compétences",
-    "experience" : "Expérience de travail",
+  nav: {
+    about: "À propos de moi",
+    education: "Formation",
+    skills: "Compétences",
+    experience: "Expérience de travail",
   },
-  "section" : {
-    "aboutSection": "<h2>À propos de moi</h2><p>Je suis actuellement en deuxième année de licence en informatique à l'Université de Strasbourg. Mon intérêt se porte sur les domaines de la bio-informatique, de la science des données et du développement de systèmes d'exploitation. J'ai déjà une petite expérience en tant que bioinformaticien, ce qui m'a convaincu que j'allais dans la bonne direction et m'a permis de mieux m'adapter à différents environnements</p>", 
-    "educationSection" : "<h2>Formation</h2><li><dt>École numéro 27</dt><dd>Petrozavodsk / 2008-2019<br>Baccalauréat en chimie et biologie<br>Compétences acquises : informatique de base, biologie</dd ><dt>Université de Strasbourg</dt><dd>Strasbourg / 2019-2022<br>Licence en biologie moléculaire et cellulaire<br>Compétences acquises : Python, MS Office, Linux, centrifugation, chromatographie, électrophorèse</dd> <br><dd>Strasbourg / 2023-maintenant (semestre d'hiver)<br>Licence en informatique<br>Compétences acquises : Python, C, Algorithmique, Javascript, HTML, CSS, algèbre linéaire, Linux</dd></li>",
-    "skillsSection": "<h2>Compétences</h2><li>   <dt>Informatique :</dt>   <dd>Python, Javascript, HTML, CSS, Haskell, C</dd>   <dt>Bioinformatique :</dt>   <dd>Services Biopython, EBI et NCBI, PDB</dd>   <dt>Science des données :</dt>   <dd>Algèbre linéaire, statistiques, R, Pandas</dd>   <dt>Langues :</dt>   <dd>Anglais (C1), français (B2+), allemand (B2), russe (natif)</dd>   <dt>Autres :</dt>   <dd>OnlyOffice, Google Office</dd></li>",
-    "experienceSection": "<h2>Mon expérience professionnelle</h2><li>   <dt>Stage en bioinformatique et biologie moléculaire</dt>   <dd>CNRS / Septembre-Décembre 2021<br> Majoritairement orienté vers la bioinformatique, ce stage m'a permis d'améliorer mes connaissances sur Python et les différents services utilisés en bioinformatique</dd>   <dt>Stage en bioinformatique</dt>   <dd>CNRS / Septembre-décembre 2022<br> J'ai travaillé dans le même groupe que la fois précédente, utilisant et améliorant les mêmes compétences et acquérant en plus des connaissances en biologie structurale</dd>   <dt>Développeur Django</dt>   <dd>Consilio / Juin-Juillet 2022<br>Ma première expérience dans le web. Nous avons développé un site pour une petite startup utilisant Django comme back-end</dd></li>",
-  }
-}
+  section: {
+    aboutSection:
+      "<h2>À propos de moi</h2> <p>Actuellement, je suis étudiant en première année de master en informatique appliquée à l'Académie Internationale de Moscou. Je m'intéresse à des domaines tels que la bioinformatique, la science des données et le développement de systèmes d'exploitation. J'ai déjà une petite expérience en tant que bioinformaticien/analyste, ce qui m'a convaincu que je suis sur la bonne voie et m'a permis de mieux m'adapter à diverses conditions.</p> ",
+    educationSection:
+      "<h2>Formation</h2><li><dt>École n° 27</dt><dd>Petrozavodsk / 2008-2019<br>Profil Chimie-Biologie<br>Compétences acquises : informatique de base, biologie</dd><dt>Université de Strasbourg</dt><dd>Strasbourg (France) / 2019-2022<br>Licence en biologie moléculaire et cellulaire<br>Compétences acquises : Python, MS Office, Linux, statistiques de base, centrifugation, chromatographie, électrophorèse</dd><br><dd>Strasbourg (France) / 2023-2024<br>Formation complémentaire en informatique<br>Compétences acquises : Python, C, algorithmique, Javascript, HTML, CSS, algèbre linéaire, Linux</dd></li>",
+    skillsSection:
+      "<h2>Compétences</h2><li><dt>Informatique :</dt><dd>Python, HTML, CSS, Haskell, C/C++</dd><dt>Bioinformatique :</dt><dd>biopython, services EBI et NCBI, PDB</dd><dt>Science des données :</dt><dd>algèbre linéaire, statistiques, R, pandas</dd><dt>Langues :</dt><dd>Anglais (C1), Français (B2+), Allemand (B2), Russe (natif)</dd><dt>Autres :</dt><dd>OnlyOffice, Google Office</dd></li>",
+    experienceSection:
+      "<h2>Expérience professionnelle</h2><li><dt>Stage en bioinformatique</dt><dd>CNRS / Septembre-Décembre 2021<br>Principalement axé sur le domaine de la bioinformatique, ce stage m'a aidé à améliorer ma compréhension de Python et de la bioinformatique.</dd><dt>Stage en bioinformatique</dt><dd>CNRS / Septembre-Décembre 2022<br>Essentiellement une continuation du stage précédent, il m'a permis d'approfondir davantage mes compétences et connaissances en biologie des protéines.</dd><dt>Développeur Django</dt><dd>Consilio / Juin-Juillet 2022<br>Ma première expérience en développement web. Nous avons développé le backend pour une petite startup.</dd></li>",
+  },
+};
+
+var ruJson = {
+  nav: {
+    about: "Обо мне",
+    education: "Образование",
+    skills: "Компетенции",
+    experience: "Опыт работы",
+  },
+  section: {
+    aboutSection:
+      '<h2>Обо мне</h2><p>В настоящее время я учусь на первом курсе магистратуры по направлению "Прикладная информатика" в Московской Международной Академии. Меня интересуют такие области, как биоинформатика, наука о данных и разработка операционных систем. У меня уже есть небольшой опыт работы биоинформатиком/аналитиком, который убедил меня в том, что я двигаюсь в правильном направлении, и позволил мне лучше адаптироваться к различным условиям</p>',
+    educationSection:
+      '<h2>Образование</h2><li><dt>Школа № 27</dt><dd>Петрозаводск / 2008-2019<br>Химико-Биологический профиль<br>Полученные компетенции : базовая информатика, биология</dd ><dt>Страсбургский Университет</dt><dd>Страсбург (Франция) / 2019-2022<br>Бакалавр по направлению "Молекулярная и клеточная биология"<br>Полученные компетенции: Python, MS Office, Linux, базовая статистика ,центрифугирование, хроматография, электрофорез</dd> <br><dd>Страсбург (Франция) / 2023-2024<br>Дополнительное образование по информатике<br>Полученные Компетенции : Python, C, алгоритмика, Javascript, HTML, CSS, линейная алгебра, Linux</dd></li>',
+    skillsSection:
+      "<h2>Компетенции</h2><li>   <dt>Информатика :</dt>   <dd>Python, HTML, CSS, Haskell, C/C++</dd>   <dt>Биоинформатика :</dt>   <dd>biopython, EBI et NCBI сервисы, PDB</dd>   <dt>Наука о данных :</dt>   <dd>линейная алгебра, статистика, R, pandas</dd>   <dt>Языки :</dt>   <dd>английский (C1), французский (B2+), немецкий (B2), русский (родной)</dd>   <dt>Другие :</dt>   <dd>OnlyOffice, Google Office</dd></li>",
+    experienceSection:
+      "<h2>Опыт работы</h2><li>   <dt>Стажировка по биоинформатике</dt>   <dd>CNRS / Сентябрь-Декабрь 2021<br>В основном сосредоточенная в области биоинформатики, эта стажировка помогла мне улучшить моё понимание Python и биоинформатики</dd>   <dt>Стажировка по биоинформатике</dt>   <dd>CNRS / Сентябрь-Декабрь 2022<br> По сути, продолжение предидущей стажировки, позволило мне дальше углубить свои навыки, а также знания о биологии белков</dd>   <dt>Django разработчик</dt>   <dd>Consilio / Июнь-Июль 2022<br>Мой первый опыт в веб. Мы разрабатывали бэкенд для небольшого стартапа</dd></li>",
+  },
+};
 
 // functions
 function setLanguage(langJson) {
@@ -51,7 +77,9 @@ switch (language) {
   case "FR":
     setLanguage(frJson);
     break;
+  case "RU":
+    setLanguage(ruJson);
   default:
-    setLanguage(enJson)
+    setLanguage(enJson);
     break;
 }
